fa1cbc8f9fb118f22aa6aacaa6a36a0dd41120b0
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-31 19:39:47 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
-        System.out.println("Kac ögrenci gireceksiniz?");
+        GradeAnalyzer gradeAnalyzer = new GradeAnalyzer();
+
+        System.out.println("Kac ögrencinin notunu gireceksiniz?");
         int numOfStudent = scanner.nextInt();
+        gradeAnalyzer.getGrades(numOfStudent);
+
+        System.out.println("Ortalama : "+ gradeAnalyzer.calculateAverage());
+        System.out.println("Maximum : "+ gradeAnalyzer.findMax());
+        System.out.println("Minumum : "+ gradeAnalyzer.findMin());
+
+        System.out.print("Alt sınır: ");
+        int minRange = scanner.nextInt();
+        System.out.print("Üst sınır: ");
+        int maxRange = scanner.nextInt();
+
+        System.out.println(minRange + " ile " + maxRange + " arasındaki öğrenci sayısı: " + gradeAnalyzer.countGradesRange(minRange, maxRange));
 
 
 
